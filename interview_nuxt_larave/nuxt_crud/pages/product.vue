76bda7bda2b739dae635a4bf202f9efbdac7f2d6
nirav00000 <niravniravnirav12345@gmail.com>
2021-08-30 14:54:31 +0530

    nuxt_laravel

---
+++ interview_nuxt_larave/nuxt_crud/pages/product.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <div >
+
+
+  my page working?<br/>
+  title => {{title}}
+  title => {{description}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      description:'',
+      title:''
+    };
+  },
+  async fetch()
+  {
+    let {data} = this.$axios({
+      method: "get",
+      url: "api/product",
+    })
+      .then((response) => {
+        let res = {...response.data}
+        this.description = res[0].description;
+        this.title = res[0].title
+         console.log(res);
+      })
+  }
+};
+</script>
