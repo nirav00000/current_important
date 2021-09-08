20a7fa2676ab87ca442d718241aee70595620454
nirav00000 <niravniravnirav12345@gmail.com>
2021-09-08 16:17:44 +0530

    role permission tested, added code in adminseeder

--- laravel_auth_permission_crud/database/seeders/CreateAdminUserSeeder.php
+++ laravel_auth_permission_crud/database/seeders/CreateAdminUserSeeder.php
@@ -1,12 +1,12 @@
 <?php
-  
+
 namespace Database\Seeders;
-  
+
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-  
+
 class CreateAdminUserSeeder extends Seeder
 {
     /**
@@ -17,17 +17,51 @@ class CreateAdminUserSeeder extends Seeder
     public function run()
     {
         $user = User::create([
-            'name' => 'Hardik Savani', 
+            'name' => 'Hardik Savani',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('123456')
         ]);
-    
+
         $role = Role::create(['name' => 'Admin']);
-     
+
         $permissions = Permission::pluck('id','id')->all();
-   
+
         $role->syncPermissions($permissions);
-     
+
         $user->assignRole([$role->id]);
+
+
+         //     $normalRole = Role::firstOrCreate(['name' => 'Normal']);
+
+    //     $permissionsCustom =
+    //     Permission::select('id')
+    //     ->where('name','=','product-list')
+    //     ->orWhere('name','=','product-delete')
+
+    //     ->get()->toArray();
+
+    //     $id_array = array_column($permissionsCustom, 'id');
+
+    //     $normalRole->syncPermissions($permissionsCustom);
+
+    //     protected function create(array $data)
+    // {
+    //     $user =  User::create([
+    //         'name' => $data['name'],
+    //         'email' => $data['email'],
+    //         'password' => Hash::make($data['password']),
+    //     ]);
+
+    //     $role = Role::select('id')
+    //     ->where('name','=','Normal')
+    //     ->get()->toArray();
+
+    //     $id_array = array_column($role, 'id');
+
+
+    //     $user->assignRole($id_array);
+
+    //     return $user;
+    // }
     }
 }
